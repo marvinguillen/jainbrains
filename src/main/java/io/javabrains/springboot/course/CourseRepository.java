efdd7d27db4699a45b8c556abadc97873f40a83a
marvin.a.guillen@gmail.com <marvin.a.guillen@gmail.com>
2021-01-06 13:01:51 -0600

    Commit changes of course.class

--- src/main/java/io/javabrains/springboot/course/CourseRepository.java
+++ src/main/java/io/javabrains/springboot/course/CourseRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface CourseRepository extends CrudRepository<Course,String> {
     
     public List<Course> FindByTopicId(String topicId);
